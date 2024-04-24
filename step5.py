51442d58d6b77bd10374e1278b9224f584b49f5a
Iskander <58907830+ClarkBrown87@users.noreply.github.com>
2024-04-24 17:13:42 +0500

    Complete task 5

---
+++ step5.py
@@ -0,0 +1,34 @@
+class Employee:
+    def __init__(self, emp_id):
+        print("Employee::__init__()")
+        self.emp_id = emp_id
+
+    def get_paid(self, hours):
+        print("Employee::get_paid()")
+
+
+class Manager(Employee):
+    PAID = 1800
+
+    def __init__(self, emp_id):
+        super().__init__(emp_id)
+
+    def get_paid(self, hours):
+        return self.PAID * hours
+
+
+class Worker(Employee):
+    PAID = 2200
+
+    def __init__(self, emp_id):
+        super().__init__(emp_id)
+
+    def get_paid(self, hours):
+        return self.PAID * hours
+
+
+manager = Manager("1")
+worker = Worker("2")
+
+print(f"Зарплата менеджера: {manager.get_paid(120)}")
+print(f"Зарплата работника: {worker.get_paid(120)}")
